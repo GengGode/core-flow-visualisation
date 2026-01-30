4690a3d1c036e34ba5ecc1fc163cd930c5e2e355
Yu SuiXian <47711102+GengGode@users.noreply.github.com>
2026-01-31 03:33:47 +0800

    添加节点工厂管理器和节点结构，支持从绝对路径注册节点组

--- source/app.cpp
+++ source/app.cpp
@@ -1,5 +1,13 @@
 #include <fmt/format.h>
 
+struct node
+{
+    ~node() = default;
+    std::string describe() const { return "I am a node"; }
+};
+
+#include "factorys.hpp"
+
 #define RUNTIME_VISUALIZER_IMPLEMENTATION
 #include <runtime-visualizer.hpp>
 
@@ -16,12 +24,39 @@ int main(int argc, char* argv[])
 
     std::cout << "Hello, World! 测试中文" << std::endl;
 
+    node_factorys node_fs;
+    node_fs.register_group_from_absolute_path("tmp/创建", []() -> std::shared_ptr<node> { return nullptr; });
+    node_fs.register_group_from_absolute_path("临时/节点/创建", []() -> std::shared_ptr<node> { return nullptr; });
+    node_fs.register_group_from_absolute_path("创建", []() -> std::shared_ptr<node> { return nullptr; });
     runtime_visualizer viz;
     viz.initialize();
-    viz.main_render([]() {
+    viz.main_render([&]() {
         ImGui::Begin("Hello, world!");
         ImGui::Text("测试中文");
         ImGui::End();
+
+        if (node_fs.root_factory->size() == 0)
+        {
+            ImGui::Text("没有可用的节点工厂");
+        }
+        else
+        {
+            node_fs.for_each(
+                [](std::vector<std::string> stack, node_factorys::stack_status status, bool menu_status, std::shared_ptr<factory_group<std::function<std::shared_ptr<node>()>>> groups) {
+                    if (menu_status == false)
+                        return false;
+                    if (status == node_factorys::stack_status::begin)
+                        return ImGui::BeginMenu(stack.back().c_str());
+                    else if (status == node_factorys::stack_status::content)
+                    {
+                        if (ImGui::MenuItem(stack.back().c_str()))
+                            groups->factory_opt.value()();
+                    }
+                    else if (status == node_factorys::stack_status::end)
+                        ImGui::EndMenu();
+                    return false;
+                });
+        }
     });
 
     viz.wait_exit();
---
+++ source/factorys.hpp
@@ -0,0 +1,214 @@
+#pragma once
+#include <algorithm>
+#include <functional>
+#include <memory>
+#include <optional>
+#include <string>
+#include <vector>
+
+template <class F> class factory_group
+{
+public:
+    using functor = F;
+    using group_ptr = std::shared_ptr<factory_group<functor>>;
+    using name_group = std::pair<std::string, group_ptr>;
+    using groups = std::vector<std::pair<std::string, std::shared_ptr<factory_group<functor>>>>;
+    using groups_ptr = std::shared_ptr<groups>;
+
+public:
+    std::optional<functor> factory_opt = std::nullopt;
+    groups_ptr sub_groups = std::make_shared<groups>();
+
+public:
+    factory_group() = default;
+    factory_group(std::optional<functor> factory_opt, groups_ptr sub_groups) : factory_opt(factory_opt), sub_groups(sub_groups) {}
+
+public:
+    bool is_sub_group() { return !sub_groups->empty(); }
+    bool is_has_factory() { return factory_opt.has_value(); }
+    bool is_empty() { return sub_groups->empty() && !factory_opt.has_value(); }
+    void factory_to_sub_group(std::string name) { sub_groups->push_back(std::make_pair(name, std::make_shared<factory_group<functor>>(factory_opt, std::make_shared<groups>()))); }
+};
+
+template <class F> class factory_group_manager
+{
+public:
+    factory_group_manager() = default;
+    ~factory_group_manager() = default;
+
+public:
+    using functor = F;
+    using factory_groups = std::vector<std::pair<std::string, std::shared_ptr<factory_group<functor>>>>;
+    std::shared_ptr<factory_groups> root_factory = std::make_shared<factory_groups>();
+
+private:
+    bool register_group_to_groups(std::string path, std::vector<std::string> paths, const std::string& name, std::shared_ptr<factory_groups> groups, functor factory)
+    {
+        auto groups_it = groups->begin();
+
+        if (path != "")
+        {
+            groups_it = std::find_if(groups->begin(), groups->end(), [path](auto& group) { return group.first == path; });
+            if (groups_it == groups->end())
+            {
+                groups->push_back(std::make_pair(path, std::make_shared<factory_group<functor>>()));
+                groups_it = groups->end() - 1;
+            }
+            groups = groups_it->second->sub_groups;
+        }
+        if (paths.size() == 0)
+        {
+            // 在该组进行插入，
+            // 1.如果是新建组，就设置为factory_opt，
+            // 2.如果已经有factory_opt但是不是sub_group就转化为sub_group再插入sub_gorup
+            // 3.如果是sub_group就插入
+            if (groups_it->second->is_empty())
+            {
+                groups_it->second->factory_opt = factory;
+                return true;
+            }
+
+            if (groups_it->second->is_has_factory())
+                groups_it->second->factory_to_sub_group(groups_it->first);
+            groups_it->second->sub_groups->push_back(std::make_pair(name, std::make_shared<factory_group<functor>>()));
+            groups_it->second->sub_groups->back().second->factory_opt = factory;
+
+            return true;
+        }
+
+        if (not groups_it->second->is_sub_group() && groups_it->second->is_has_factory())
+        {
+            groups_it->second->factory_to_sub_group(groups_it->first);
+        }
+
+        auto next_path = paths[0];
+        paths.erase(paths.begin());
+        return register_group_to_groups(next_path, paths, name, groups, factory);
+    }
+
+    bool register_groups_to_groups(std::string path, std::vector<std::string> paths, std::shared_ptr<factory_groups> groups, std::shared_ptr<factory_groups> sub_groups)
+    {
+        auto groups_it = groups->begin();
+
+        if (path != "")
+        {
+            groups_it = std::find_if(groups->begin(), groups->end(), [path](auto& group) { return group.first == path; });
+            if (groups_it == groups->end())
+            {
+                groups->push_back(std::make_pair(path, std::make_shared<factory_group<functor>>()));
+                groups_it = groups->end() - 1;
+            }
+            groups = groups_it->second->sub_groups;
+        }
+        if (paths.size() == 0)
+        {
+            // 在该组进行插入，
+            // 1.如果是新建组，就设置为factory_opt，
+            // 2.如果已经有factory_opt但是不是sub_group就转化为sub_group再插入sub_gorup
+            // 3.如果是sub_group就插入
+            if (groups_it->second->is_empty())
+            {
+                groups_it->second->sub_groups = sub_groups;
+                return true;
+            }
+
+            if (groups_it->second->is_has_factory())
+                groups_it->second->factory_to_sub_group(groups_it->first);
+            groups_it->second->sub_groups->push_back(std::make_pair(sub_groups->begin()->first, std::make_shared<factory_group<functor>>()));
+            groups_it->second->sub_groups->back().second->sub_groups = sub_groups;
+
+            return true;
+        }
+
+        if (not groups_it->second->is_sub_group() && groups_it->second->is_has_factory())
+        {
+            groups_it->second->factory_to_sub_group(groups_it->first);
+        }
+
+        auto next_path = paths[0];
+        paths.erase(paths.begin());
+        return register_node_groups_to_groups(next_path, paths, groups, sub_groups);
+    }
+
+public:
+    void register_group(std::shared_ptr<factory_groups> root_groups, const std::vector<std::string>& paths, functor factory)
+    {
+        std::shared_ptr<factory_groups> groups = root_groups;
+        auto paths_copy = paths;
+        auto path = paths_copy[0];
+        auto name = paths.back();
+        paths_copy.erase(paths_copy.begin());
+        register_group_to_groups(path, paths_copy, name, groups, factory);
+    }
+    void register_group(std::shared_ptr<factory_groups> root_groups, const std::vector<std::string>& paths, std::shared_ptr<factory_groups> sub_groups)
+    {
+        std::shared_ptr<factory_groups> groups = root_groups;
+        auto paths_copy = paths;
+        auto path = paths_copy[0];
+        paths_copy.erase(paths_copy.begin());
+        register_groups_to_groups(path, paths_copy, groups, sub_groups);
+    }
+    void register_group_from_absolute_path(const std::string absolute_path, functor factory)
+    {
+        std::vector<std::string> paths;
+        std::string path;
+        for (auto& c : absolute_path)
+        {
+            if (c == '/')
+            {
+                if (path != "")
+                    paths.push_back(path);
+                path = "";
+            }
+            else
+            {
+                path += c;
+            }
+        }
+        if (path != "")
+            paths.push_back(path);
+        register_group(root_factory, paths, factory);
+    }
+    void register_group_from_factorys(std::vector<std::pair<std::string, functor>> factorys)
+    {
+        for (auto& [path, factory] : factorys)
+            register_group_from_absolute_path(root_factory, path, factory);
+    }
+
+    enum class stack_status
+    {
+        begin,
+        content,
+        end
+    };
+
+    void for_each_group(std::shared_ptr<factory_groups> root_groups, std::function<bool(std::vector<std::string>, stack_status, bool, std::shared_ptr<factory_group<functor>>)> pred,
+                        std::vector<std::string> stack = {}, bool last_menu_status = true)
+    {
+        if (!root_groups)
+            return;
+        for (auto& [path, groups] : *root_groups)
+        {
+            if (groups->sub_groups->size() == 0)
+            {
+                stack.push_back(path);
+                pred(stack, stack_status::content, last_menu_status, groups);
+                stack.pop_back();
+                continue;
+            }
+            stack.push_back(path);
+            auto menu_status = pred(stack, stack_status::begin, last_menu_status, groups);
+            for_each_group(groups->sub_groups, pred, stack, menu_status);
+            pred(stack, stack_status::end, menu_status, groups);
+            stack.pop_back();
+        }
+    }
+    void for_each(std::function<bool(std::vector<std::string>, stack_status, bool, std::shared_ptr<factory_group<functor>>)> pred, std::vector<std::string> stack = {},
+                        bool last_menu_status = true)
+    {
+        for_each_group(root_factory, pred, stack, last_menu_status);
+    }
+};
+
+struct node;
+using node_factorys = factory_group_manager<std::function<std::shared_ptr<node>()>>;
